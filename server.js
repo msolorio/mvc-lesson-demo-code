41d3d3e7af6fa366d3d20c7e56a6cbcdc74f3325
msolorio <msolorio.github@gmail.com>
2021-07-29 18:35:37 -0700

    Adds demo code for mvc lesson

--- server.js
+++ server.js
@@ -1,75 +1,52 @@
-// Node - An environment that allows us to run our JavaScript outside of a browser and gives us some helpful server capabilities.
-
-// Express - A tool / package to help us build our server application.
-
-// NPM - Node Package Manager
-
-// package.json - A file that keeps a running list of all of our packages
-
-// node_modules - A directory that holds all of our packages
-
-// nodemon - Node Monitor - Listens for when we save our file and restarts the node process (restarts our server)
-
 ////////////////////// CONFIGURATION /////////////////////
-
-// Pulling in the express package into this file
 const express = require('express');
-
-// Creating an instance of an express app
 const app = express();
-
 const port = 4000;
+app.set('view engine', 'ejs');
 
-// Temperary, simulated database
-//              0        1       2
-const fruits = ['apple', 'kiwi', 'orange'];
+
+const fruits = require('./models/fruit_model');
 
 
 /////////////////////////// ROUTES ///////////////////////
-
-// Listen for requests on the '/' route, and when they are recieved
-// call this callback function
 app.get('/', (request, response) => {
   response.send('Welcome to the Fruits App');
-})
+});
+
+
 
 app.get('/about', (request, response) => {
   response.send('All about my website');
 })
 
 
+
+// app.get('/fruits', (request, response) => {
+//   response.send(fruits);
+// })
+
+
+app.get('/fruits', (request, response) => {
+  // response.send(fruits);
+  response.render('index.ejs', {
+    allFruits: fruits
+  });
+})
+
+
+
 app.get('/fruits/:fruitIndex', (request, response) => {
-  response.send(fruits[request.params.fruitIndex]);
+  // response.send(fruits[request.params.fruitIndex]);
+  response.render('show.ejs', {
+    oneFruit: fruits[request.params.fruitIndex]
+  });
 });
 
 
-// app.get('/fruits/:fruitIndex/:clientName', (request, response) => {
-//   console.log(fruits[request.params.fruitIndex]);
 
-//   const fruitIndex = request.params.fruitIndex;
-//   const clientName = request.params.clientName;
 
-//   const message = `Hello ${clientName}, here is your ${fruits[fruitIndex]}`
 
-//   response.send(message);
-// });
 
-// app.get('/fruits/0', (request, response) => {
-//   response.send(fruits[0]);
-// })
-
-// app.get('/fruits/1', (request, response) => {
-//   response.send(fruits[1]);
-// })
-
-// app.get('/fruits/2', (request, response) => {
-//   response.send(fruits[2]);
-// })
-
-///////////////////// STARTING THE SERVER /////////////////
-// Start up our server
-// Start the server on the specified port
-// After it starts, call the callback function
 app.listen(port, () => {
   console.log(`Your server is running on port: ${port} 🚀`);
 });
